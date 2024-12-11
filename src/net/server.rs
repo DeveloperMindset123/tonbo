4df4769a7c8c3ac4901ce997884791128e7a57c1
Gwo Tzu-Hsing <gotzehsing@gmail.com>
2024-12-11 12:22:57 +0800

    fix

--- src/net/server.rs
+++ src/net/server.rs
@@ -1,19 +1,21 @@
-use std::io::Cursor;
-use std::ops::Bound;
-use std::pin::Pin;
+use crate::executor::tokio::TokioExecutor;
+use crate::net::proto::tonbo_rpc_server::TonboRpc;
+use crate::net::proto::{Empty, GetReq, GetResp, InsertReq, RemoveReq, ScanReq, ScanResp};
+use crate::record::{Column, DynRecord};
+use crate::serdes::{Decode, Encode};
+use crate::DB;
 use async_stream::stream;
 use bytes::BufMut;
 use futures_core::Stream;
 use futures_util::StreamExt;
+use std::io::Cursor;
+use std::ops::Bound;
+use std::pin::Pin;
+use std::sync::Arc;
 use tonic::{Code, Request, Response, Status};
-use crate::DB;
-use crate::executor::tokio::TokioExecutor;
-use crate::net::proto::{Empty, GetReq, GetResp, InsertReq, RemoveReq, ScanReq, ScanResp};
-use crate::net::proto::tonbo_rpc_server::TonboRpc;
-use crate::record::{Column, DynRecord};
-use crate::serdes::{Decode, Encode};
+
 struct TonboService {
-    inner: DB<DynRecord, TokioExecutor>,
+    inner: Arc<DB<DynRecord, TokioExecutor>>,
     primary_key_index: usize,
 }
 
@@ -21,16 +23,21 @@ struct TonboService {
 impl TonboRpc for TonboService {
     async fn get(&self, request: Request<GetReq>) -> Result<Response<GetResp>, Status> {
         let mut req = request.into_inner();
-        let key = Column::decode(&mut Cursor::new(&mut req.key)).await
+        let key = Column::decode(&mut Cursor::new(&mut req.key))
+            .await
             .map_err(|e| Status::new(Code::Internal, e.to_string()))?;
 
-        let tuple = self.inner
+        let tuple = self
+            .inner
             .get(&key, |e| Some(e.get().columns))
             .await
             .map_err(|e| Status::new(Code::Internal, e.to_string()))?;
         let record = if let Some(tuple) = tuple {
             let mut bytes = Vec::new();
-            tuple.encode(&mut Cursor::new(&mut bytes)).await.map_err(|e| Status::new(Code::Internal, e.to_string()))?;
+            tuple
+                .encode(&mut Cursor::new(&mut bytes))
+                .await
+                .map_err(|e| Status::new(Code::Internal, e.to_string()))?;
             Some(bytes)
         } else {
             None
@@ -38,10 +45,11 @@ impl TonboRpc for TonboService {
         Ok(Response::new(GetResp { record }))
     }
 
-    type ScanStream = Pin<Box<dyn Stream<Item = Result<ScanResp, Status>> + Send + Sync + 'static>>;
+    type ScanStream = Pin<Box<dyn Stream<Item = Result<ScanResp, Status>> + Send + 'static>>;
 
     async fn scan(&self, request: Request<ScanReq>) -> Result<Response<Self::ScanStream>, Status> {
         let mut req = request.into_inner();
+        let this = Arc::clone(&self.inner);
 
         let stream = stream! {
             let min = Bound::<Column>::decode(&mut Cursor::new(&mut req.min)).await
@@ -49,10 +57,9 @@ impl TonboRpc for TonboService {
             let max = Bound::<Column>::decode(&mut Cursor::new(&mut req.max)).await
                 .map_err(|e| Status::new(Code::Internal, e.to_string()))?;
 
-            let mut stream = self
-                .inner
+            let mut stream = std::pin::pin!(this
                 .scan((min.as_ref(), max.as_ref()), |e| Some(e.get().columns))
-                .await;
+                .await);
             while let Some(entry) = stream.next().await {
                 let Some(columns) = entry.map_err(|e| Status::new(Code::Internal, e.to_string()))? else { continue };
                 let mut bytes = Vec::new();
@@ -61,12 +68,16 @@ impl TonboRpc for TonboService {
             }
         };
 
-        Ok(Response::new(Box::pin(stream)))
+        Ok(Response::new(Box::pin(stream)
+            as Pin<
+                Box<dyn Stream<Item = Result<ScanResp, Status>> + Send + 'static>,
+            >))
     }
 
     async fn insert(&self, request: Request<InsertReq>) -> Result<Response<Empty>, Status> {
         let mut req = request.into_inner();
-        let record = Vec::<Column>::decode(&mut Cursor::new(&mut req.record)).await
+        let record = Vec::<Column>::decode(&mut Cursor::new(&mut req.record))
+            .await
             .map_err(|e| Status::new(Code::Internal, e.to_string()))?;
 
         self.inner
@@ -78,7 +89,8 @@ impl TonboRpc for TonboService {
 
     async fn remove(&self, request: Request<RemoveReq>) -> Result<Response<Empty>, Status> {
         let mut req = request.into_inner();
-        let column = Column::decode(&mut Cursor::new(&mut req.key)).await
+        let column = Column::decode(&mut Cursor::new(&mut req.key))
+            .await
             .map_err(|e| Status::new(Code::Internal, e.to_string()))?;
         self.inner
             .remove(column)
@@ -88,8 +100,8 @@ impl TonboRpc for TonboService {
     }
 
     async fn flush(&self, _: Request<Empty>) -> Result<Response<Empty>, Status> {
-       self.inner
-           .flush()
+        self.inner
+            .flush()
             .await
             .map_err(|e| Status::new(Code::Internal, e.to_string()))?;
         Ok(Response::new(Empty {}))
